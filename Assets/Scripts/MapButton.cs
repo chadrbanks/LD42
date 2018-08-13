174a664f0abcd7d9b025fc626d2f5cac6b4dd336
Chad Banks <crbanks1@hfcc.edu>
2018-08-13 09:25:19 -0400

    Lots of rushed work.

--- Assets/Scripts/MapButton.cs
+++ Assets/Scripts/MapButton.cs
@@ -6,10 +6,39 @@ using UnityEngine.SceneManagement;
 public class MapButton : MonoBehaviour
 {
     public int planetid;
+    public TextMesh ps;
 
 	void Start ()
     {
 		
+    }
+
+    void OnMouseOver()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1.0f);
+
+        if (planetid == 1)
+        {
+            ps.text = "P1";
+        }
+        else if (planetid == 2)
+        {
+            ps.text = "P2";
+        }
+        else if (planetid == 3)
+        {
+            ps.text = "P3";
+        }
+        else if (planetid == 4)
+        {
+            ps.text = "MISSINGNO";
+        }
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0f);
+        ps.text = "";
     }
 
     void OnMouseDown()
